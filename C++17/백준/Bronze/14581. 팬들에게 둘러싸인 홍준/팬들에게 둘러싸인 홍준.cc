8ea426c9db39db014013bec9d0a201a0b4a458bf
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-03-08 02:20:11 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string a;
+    cin >> a;
+    
+    cout << ":fan::fan::fan:\n" << ":fan::" << a << "::fan:\n" << ":fan::fan::fan:\n";
+}
