131e9e9be1679f53eca297bce1d603cafc5dab12
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-03-07 22:37:27 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cout << ".  .   ." << endl << "|  | _ | _. _ ._ _  _" << endl << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+}
