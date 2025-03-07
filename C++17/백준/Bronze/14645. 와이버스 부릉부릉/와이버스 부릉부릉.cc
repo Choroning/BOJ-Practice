d5fe45b8f9bd753bf3fae89a22bb6fb6cd919d98
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-03-08 02:25:57 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int nStop, pStart, a, b;
+    
+    cin >> nStop >> pStart;
+    for (int i = 0; i < nStop; i++) {
+        cin >> a >> b;
+    }
+    cout << "비와이" << endl;
+}
