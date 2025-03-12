878fbf3853bb5e8c8203a7ce0a7b34c65067c230
Cheolwon Park <152289594+Choroning@users.noreply.github.com>
2025-03-12 17:07:58 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/15829. Hashing/Hashing.cc
+++ C++17/백준/Bronze/15829. Hashing/Hashing.cc
@@ -6,17 +6,23 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int l, res = 0;
+
+    long long l;
     cin >> l;
-    
     cin.ignore();
     
     string sen;
     getline(cin, sen);
-    
+
+    const long long MOD = 1234567891;
+    long long res = 0;
+    long long pow31 = 1;
+
     for (int i = 0; i < l; i++) {
-        res += (sen[i] - 'a' + 1) * pow(31, i);
+        long long val = (sen[i] - 'a' + 1);
+        res = (res + (val * pow31) % MOD) % MOD;
+        pow31 = (pow31 * 31) % MOD;
     }
-    cout << res % 1234567891 << endl;
+    cout << res << '\n';
+    return 0;
 }
